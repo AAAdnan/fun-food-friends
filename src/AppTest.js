b991f579bda82ba2a3f4c4b3a86be28dd3ecb8c1
Adnan Ahmed <adnan.ahmed01@gmail.com>
2019-12-07 15:59:35 +0000

    added security

---
+++ src/AppTest.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function App() {
+    const [currentItem, setCurrentItem] =  useState('');
+    const [userName, setUsername] =  useState('');
+    const [items, setCurrentItems] =  useState([]);
+    const [user, setCurrentUser] =  useState(null);
+
+}
+
+
+this.state = {
+    currentItem: '',
+    username: '',
+    items: [],
+    user: null
+  }
